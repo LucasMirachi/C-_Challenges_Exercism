9c1747bc426f46b5799a5daa19b4b4b34a627d80
lucasmirachi <lucas.mirachi@gmail.com>
2021-11-17 00:05:16 -0300

    Added challange #01-Hello, World!

---
+++ 01_hello-world/hello-world.cpp
@@ -0,0 +1,14 @@
+//iostream a header file that contains functions for input/output operations
+//(cin and cout)
+#include <iostream>
+#include <cstdio>
+
+
+// Using everything from the 'std' namespace lets us write 'string' instead of
+//'std::string'.
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
